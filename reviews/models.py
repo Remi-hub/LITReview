e92cc1aacb0d63677494053819d387b02620853c
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-08-25 18:51:47 +0200

    import Ticket from tickets.models

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
+from tickets.models import Ticket
 
 
 # Create your models here.
