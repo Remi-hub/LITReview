51aa06cdc59d8e830d5ce0296fd5611e0ff9e31d
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-08-25 18:04:29 +0200

    Add class Userfollow

--- userfollows/models.py
+++ userfollows/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from django.db import models
 
 # Create your models here.
+
+
+class Userfollow(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+
+
+
