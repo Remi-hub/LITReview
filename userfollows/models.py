d18774232c7ac1358a41fd38300e1712055653cc
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-08-25 18:14:16 +0200

    Add class Meta for Userfollow

--- userfollows/models.py
+++ userfollows/models.py
@@ -12,5 +12,8 @@ class Userfollow(models.Model):
     followed_user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
 
+    class Meta:
+        unique_together = ['user', 'followed_user']
+
 
 
