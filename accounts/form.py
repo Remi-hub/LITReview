5625ec6940794465d14b0427447c63f89126c500
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-08-30 18:20:08 +0200

    Add form.py -- adding email field

---
+++ accounts/form.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm, UsernameField
+from django.contrib.auth.models import User
+
+
+class UserCreationForm(BaseUserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email")
+        field_classes = {'username': UsernameField}
