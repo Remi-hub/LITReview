b62f319722447bacbde1d2f9a8a5851594898622
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-08-31 17:47:05 +0200

    create views for tickets

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.views.generic.detail import DetailView
+from tickets.models import Ticket
+from django.views.generic import ListView
+from django.urls import reverse_lazy
+
+
 
 # Create your views here.
+
+class CreateTicket(generic.CreateView):
+    model = Ticket
+    success_url = reverse_lazy('home')
+    template_name = 'tickets/create_ticket.html'
+    fields = ['title', 'description', 'image']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+
