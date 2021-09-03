e81975728ef7be98824d0d3fe1b694d26a13f373
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-03 20:06:46 +0200

    Add path user_autocomplete

--- userfollows/urls.py
+++ userfollows/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from userfollows.views import FollowsView
+from .views import UserAutocomplete
+# from your_countries_app.views import CountryAutocomplete
 
 
 urlpatterns = [
-    path('follows/', FollowsView.as_view(), name='follows')
+    path('follows/', FollowsView.as_view(), name='follows'),
+    path('user_autocomplete/', UserAutocomplete.as_view(), name='user-autocomplete')
 ]
