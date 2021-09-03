320d4128211788fb6341b0d4e809dca44a0eecc2
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-03 13:10:18 +0200

    renamed LOGIN_REDIRECT_URL

--- LITReview/settings.py
+++ LITReview/settings.py
@@ -134,14 +134,14 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'flux'
+LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
 
-MEDIA_ROOT = BASE_DIR / "LITReview"/'media'
+MEDIA_ROOT = BASE_DIR / 'LITReview'/'media'
 MEDIA_URL = '/media/'
 
 
