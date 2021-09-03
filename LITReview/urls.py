41c4baeda9783f9235edc07d3ea0b601d01f31a6
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-03 20:07:08 +0200

    update urls.py

--- LITReview/urls.py
+++ LITReview/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('flux/', TemplateView.as_view(template_name='flux.html'), name='flux'),
     path('tickets/', include('tickets.urls')),
+    path('userfollows/', include('userfollows.urls'))
 ]
