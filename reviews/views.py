1d2aec29df6b51cb6b19a1da4a548824c62ff9be
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-07 13:15:22 +0200

    add class ReviewsView for view.py (reviews)

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from reviews.models import Review
+
 
 # Create your views here.
+
+
+class ReviewsView(LoginRequiredMixin, generic.CreateView):
+
+    model = Review
+    template_name = 'reviews/create_reviews.html'
+    fields = ['headline', 'body', 'rating']
+    success_url = reverse_lazy('flux')
+
+
