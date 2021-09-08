5a6e4b7986d3887558966033b9d0e61fc0a7906a
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-08 13:46:04 +0200

    changed the reverse_lazy parameter

--- tickets/views.py
+++ tickets/views.py
@@ -6,15 +6,22 @@ from tickets.models import Ticket
 
 
 # Create your views here.
+def show_tickets():
+    tickets = Ticket.objects.order_by('-created_date')
+
+
 class CreateTicket(LoginRequiredMixin, generic.CreateView):
     model = Ticket
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('flux')
     template_name = 'tickets/create_ticket.html'
     fields = ['title', 'description', 'image']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+
 
 
 
