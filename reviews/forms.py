96c0e9c3c65769a48041c1cf19cea2b7b2680cc2
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-08 13:44:48 +0200

    add class Meta for ReviewForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,16 @@
+from django import forms
 from reviews.models import Review
-from django.forms import ModelForm
+from django.forms import ModelForm, TypedChoiceField, RadioSelect
+
+
+choices = (
+    (0, '0'),
+    (1, '1')
+)
+
 
 class ReviewForm(ModelForm):
-    pass
+    class Meta:
+        model = Review
+        fields = 'headline', 'rating', 'body'
 
-# TODO suivre exemple avec classe meta userfollows/forms
