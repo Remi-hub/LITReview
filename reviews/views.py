cda04df9f5b78262437ba04b55bc238f0b986cbf
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-08 13:47:00 +0200

    add function review_create_view

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,53 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from reviews.models import Review
-
+from reviews.forms import ReviewForm
+from tickets.forms import TicketForm
+from tickets.views import CreateTicket
 
 # Create your views here.
 
 
-class ReviewsView(LoginRequiredMixin, generic.CreateView):
+# class ReviewsView(LoginRequiredMixin, generic.CreateView):
+#
+#     model = Review
+#     template_name = 'reviews/create_reviews.html'
+#     fields = ['headline', 'body', 'rating']
+#     success_url = reverse_lazy('flux')
 
-    model = Review
-    template_name = 'reviews/create_reviews.html'
-    fields = ['headline', 'body', 'rating']
-    success_url = reverse_lazy('flux')
+# class ReviewsView(LoginRequiredMixin, generic.CreateView):
+#     model = Review,
+#     form_class = ReviewForm
+#     template_name = 'reviews/create_reviews.html'
 
 
+def review_create_view(request):
+    if request.method == 'GET':
+        form_review = ReviewForm()
+        form_ticket = TicketForm()
+        html = 'reviews/create_reviews.html'
+        context = {
+            'form_review': form_review, 'form_ticket': form_ticket
+        }
+        return render(request, html, context)
+
+    elif request.method == 'POST':
+        form_review = ReviewForm(data=request.POST)
+        form_ticket = TicketForm(data=request.POST)
+        html = 'reviews/create_reviews.html'
+        context = {
+            'form_review': form_review, 'form_ticket': form_ticket
+        }
+        
+        if form_review.is_valid() and form_ticket.is_valid():
+            form_ticket.instance.user = request.user
+            ticket = form_ticket.save()
+            form_review.instance.ticket = ticket    #ticket dans le model review
+            form_review.instance.user = request.user
+            form_review.save()
+
+            return redirect('flux')
+
+        return render(request, html, context)
