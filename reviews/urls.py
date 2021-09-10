c0ea809aa4fcedbbac03a5c30645c6387b0c5897
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-10 13:22:34 +0200

    add review_create_view_with_ticket

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from reviews.views import review_create_view
+from reviews.views import review_create_view_with_ticket
 
 
 
 urlpatterns = [
     path('create_review/', review_create_view, name='create_reviews'),
+    path('create_review_with_ticket/<int:ticket_id>/', review_create_view_with_ticket, name='create_review_with_ticket'),
+
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -6,6 +6,8 @@ from reviews.models import Review
 from reviews.forms import ReviewForm
 from tickets.forms import TicketForm
 from tickets.views import CreateTicket
+from tickets.models import Ticket
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -52,3 +54,31 @@ def review_create_view(request):
             return redirect('flux')
 
         return render(request, html, context)
+
+
+
+def review_create_view_with_ticket(request, ticket_id):
+
+    if request.method == 'GET':
+        form_review = ReviewForm()
+        ticket = get_object_or_404(Ticket, id=ticket_id)
+        html = 'reviews/create_review_with_ticket.html'
+        context = {
+            'form_review': form_review, 'ticket': ticket
+        }
+        return render(request, html, context)
+
+    elif request.method == 'POST':
+        form_review = ReviewForm(data=request.POST, files=request.FILES)
+        html = 'reviews/create_review_with_ticket.html'
+        context = {
+            'form_review': form_review,
+        }
+
+        if form_review.is_valid():
+            form_review.instance.user = request.user
+            form_review.save()
+            return redirect('flux')
+
+        return render(request, html, context)
+
