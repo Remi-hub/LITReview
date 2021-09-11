209402697767ccca9099daf14e3b5498dc28e471
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-11 11:37:47 +0200

    add page to see your own posts and tickets

--- flux/urls.py
+++ flux/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from flux.views import events_view
+from flux.views import events_view, personal_events_view
 
 urlpatterns = [
     path('', events_view, name='flux'),
+    path('my_posts', personal_events_view,name='my_posts')
 ]
--- flux/views.py
+++ flux/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from tickets.models import Ticket
 from reviews.models import Review
+from django.contrib.auth.mixins import LoginRequiredMixin
 from itertools import chain
 
 # Create your views here.
 
-
-"""
-Afficher les tickets & les reviews 
-nous allons devoir faire une query sur ces objets
-faire une boucle sur le template pour parcourir tous nos objets ( tickets & reviews )
-
-"""
 
 def events_view(request):
     html = 'flux/flux.html'
     context = {
         'tickets': Ticket.objects.all().order_by('-time_created'),
         'reviews': Review.objects.all().order_by('-time_created')
+    }
+
+    return render(request, html, context)
+
+
+def personal_events_view(request):
+    html = 'flux/my_posts.html'
+    current_user = request.user
+    context = {
+        'tickets': Ticket.objects.filter(user=current_user),
+        'reviews': Review.objects.filter(user=current_user)
     }
 
     return render(request, html, context)
