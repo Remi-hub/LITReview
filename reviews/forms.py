911336de4676880439e7add3e53dba0d05be7007
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-09-11 11:38:14 +0200

    add rating radio buttons

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 from reviews.models import Review
-from django.forms import ModelForm, TypedChoiceField, RadioSelect
-
-
-choices = (
-    (0, '0'),
-    (1, '1')
-)
+from django.forms import ModelForm, TypedChoiceField, RadioSelect, Select
+from django.utils.safestring import mark_safe
 
 
 class ReviewForm(ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super(ReviewForm, self).__init__(*args, **kwargs)
+    #     self.fields['rating'].empty_label = None
+
     class Meta:
         model = Review
-        fields = 'headline', 'rating', 'body'
+        fields = ('headline', 'rating', 'body')
+        widgets = {
+            'rating': RadioSelect
+        }
 
--- reviews/models.py
+++ reviews/models.py
@@ -7,14 +7,28 @@ from tickets.models import Ticket
 
 # Create your models here.
 
+STAR_CHOICES = (
+    (0, "0"),
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5")
+)
+
+
 class Review(models.Model):
+
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
         verbose_name='Note',
         # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)])
-    headline = models.CharField(verbose_name='Titre', max_length=128)
-    body = models.CharField(verbose_name='Critique', max_length=8192, blank=True)
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        choices=STAR_CHOICES,
+    )
+    headline = models.CharField(verbose_name='Titre de la critique', max_length=128)
+    body = models.TextField(verbose_name='Commentaire', max_length=8192, blank=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
+
